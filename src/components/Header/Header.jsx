b3985cb27218948655325b469cee2fcc15418c0e
Sofiia Novychenko <milakotiy@gmail.com>
2025-07-23 16:07:46 +0200

    finish header, add loader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import css from './Header.module.css';
+import { NavLink } from 'react-router-dom';
+import clsx from 'clsx';
+
+const buildLinkClass = ({ isActive }) => {
+  return clsx(css.link, isActive && css.active);
+};
 
 function Header() {
-  return <div className="container">Header</div>;
+  return (
+    <header className={css.header}>
+      <nav className={css.headerNav}>
+        <a href="/" className={css.logo} aria-label="Site logo">
+          <svg width="102" height="16">
+            <use xlinkHref="/sprite.svg#icon-RentalCar"></use>
+          </svg>
+        </a>
+        <ul className={css.navList}>
+          <li className={css.item}>
+            <NavLink to="/" className={buildLinkClass}>
+              Home
+            </NavLink>
+          </li>
+          <li className={css.item}>
+            <NavLink to="/catalog" className={buildLinkClass}>
+              Catalog
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
