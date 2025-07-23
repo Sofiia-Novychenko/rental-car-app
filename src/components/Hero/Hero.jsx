534fe42c290b4a47f0dfe614b1885cbd0b072108
Sofiia Novychenko <milakotiy@gmail.com>
2025-07-23 14:55:34 +0200

    add hero section

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,30 @@
 import React from 'react';
+import css from './Hero.module.css';
+import { useNavigate } from 'react-router-dom';
 
 function Hero() {
-  return <div>Hero</div>;
+  const navigate = useNavigate();
+  return (
+    <section className={css.hero}>
+      <div className="container">
+        <div className={css.heroContentContainer}>
+          <h1 className={css.mainTitle}>Find your perfect rental car</h1>
+          <p className={css.subTitle}>
+            Reliable and budget-friendly rentals for any journey
+          </p>
+          <button
+            className={css.btnBlue}
+            type="button"
+            onClick={() => {
+              navigate('/catalog');
+            }}
+          >
+            View Catalog
+          </button>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Hero;
